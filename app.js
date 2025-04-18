367c4e3c7b485f1a0a5db8822d83e1c305764a79
noinvis <lazizbek.sn@gmail.com>
2025-04-19 03:54:41 +0500

    First

---
+++ app.js
@@ -0,0 +1,133 @@
+{
+    // 1 - task
+    // let n = 10 // O'zgaruvchini manfiyga o'zgartirib tekshirsangiz bo'ladi
+    // if(n > 0){
+    //     n = n + 1
+    // }
+    // console.log(n);
+}
+{
+    // 2 - task
+    // let n = 10 // O'zgaruvchini manfiyga o'zgartirib tekshirsangiz bo'ladi
+    // if(n > 0){
+    //     n = n + 1
+    //     console.log(n);
+    // }
+    // else{
+    //     n = n - 2
+    //     console.log(n);
+    // }
+}
+{
+    // 3 - task
+    // let n = 0 // O'zgaruvchini manfiyga o'zgartirib tekshirsangiz bo'ladi
+    // if(n > 0){
+    //     n = n + 1
+    //     console.log(n);
+    // }
+    // else if(n == 0){
+    //     n = n + 10
+    //     console.log(n);
+    // }
+    // else{
+    //     n = n - 2
+    //     console.log(n);
+    // }
+}
+{
+    // 4 - task
+    // let a = 3, b = -1, c = 5;
+    // let count = 0;
+    // if (a > 0) {
+    //     count++
+    // }
+    // if (b > 0) {
+    //     count++
+    // }
+    // if (c > 0) {
+    //     count++;
+    // }
+    // console.log("Musbat sonlar soni:", count);
+}
+{
+    // 5 - task
+    // let a = 1, b = -2, c = 0;
+    // let musbat = 0, manfiy = 0;
+    // if (a > 0) {
+    //     musbat++
+    // }
+    // else if (a < 0) {
+    //     manfiy++
+    // }
+    // if (b > 0) {
+    //     musbat++
+    // }
+    // else if (b < 0) {
+    //     manfiy++
+    // }
+    // if (c > 0) {
+    //     musbat++
+    // }
+    // else if (c < 0) {
+    //     manfiy++
+    // }
+    // console.log("Musbat:", musbat, "Manfiy:", manfiy);
+}
+{
+    // 6 - task
+    // let a = 7, b = 3;
+    // let katta;
+    // if (a > b) {
+    //     katta = a
+    // }
+    // else {
+    //     katta = b
+    // }
+    // console.log("Katta son:", katta);
+}
+{
+    // 7 - task
+    // let a = 5, b = 9;
+    // let tartib;
+    // if (a < b) {
+    //     tartib = 1
+    // }
+    // else {
+    //     tartib = 2
+    // }
+    // console.log("Kichik sonning tartib raqami:", tartib);
+}
+{
+    // 8 - task
+    // let a = 10, b = 4;
+    // if (a > b) {
+    //     console.log(a, b);
+    // }
+    // else {
+    //     console.log(b, a);
+    // }
+}
+{
+    // 9 - task 
+    // let A = 8.3, B = 4.2;
+    // if (A > B) {
+    //     let temp = A;
+    //     A = B;
+    //     B = temp;
+    // }
+    // console.log("A:", A, "B:", B);
+}
+{
+    // 10 - task
+    // let A = 3, B = 5;
+    // if (A !== B) {
+    //     let sum = A + B;
+    //     A = sum;
+    //     B = sum;
+    // }
+    // else {
+    //     A = 0;
+    //     B = 0;
+    // }
+    // console.log("A:", A, "B:", B);
+}
